6fd6046756637f97a11995e3f67d0d05f6771cf7
pdavinunes <p.davi.nunes@gmail.com>
2020-08-07 18:49:50 -0300

    Criando migrations

---
+++ server/src/routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/connections', );
+router.post('/connections', );
+router.get('/classes', );
+router.post('/classes', );
+
+export default router;
